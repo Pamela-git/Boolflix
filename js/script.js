94f076235664623a6a938e7b075903a8df3fd53e
Pamela Ladu <pamela.ladu@gmail.com>
2020-12-04 17:45:15 +0100

    commenti + pulizia codice

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// variabili chiamata api
 const urlMovie = "https://api.themoviedb.org/3/search/movie?";
 const urlSerie = "https://api.themoviedb.org/3/search/tv?";
 const apiKey = "api_key=54a6d59873b14d17cd7ea35eed688e38&query=";
@@ -5,31 +6,30 @@ const apiKey = "api_key=54a6d59873b14d17cd7ea35eed688e38&query=";
 var app = new Vue({
   el: "#root",
   data: {
-    active: true,
-    hide: false,
-    showIntro: true,
-    posizione: 0,
-    urlImg: 'https://image.tmdb.org/t/p/w342',
-    queryInput: "",
-    libreriaMovie: [],
-    libreriaSerie: []
+    showIntro: true, //show titolo intro
+    urlImg: 'https://image.tmdb.org/t/p/w342', //url fissa immagini
+    queryInput: "", //input utente
+    libreriaMovie: [], //arrayfilm
+    libreriaSerie: [] //arrayserie
   },
   methods: {
+    //chiamata film con input utente
     ricerca: function() {
-      // let search = this.queryInput;
       axios.get(urlMovie + apiKey + this.queryInput)
       .then(risposta => {
         var movie = risposta.data.results;
         this.libreriaMovie = movie;
       });
-
+      // chiamata serie con input utente
       axios.get(urlSerie + apiKey + this.queryInput)
       .then(result => {
         var serie = result.data.results;
         this.libreriaSerie = serie;
 
       });
+      // mettere titolo intro non visibile
       this.showIntro = false;
+      // azzerare barra ricerca
       this.queryInput = "";
     },
   }
